2669e836174e57fda81a1682bc5a6379735efa58
abba Malainine <abba.malainine@gmail.com>
2022-07-24 18:34:04 +0100

    add test approval for standard item : standard_item_quality_decrease_sellin_decrease_every_day

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -13,5 +13,15 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
+    
+    @Test 
+    void standard_item_quality_decrease_sellin_decrease_every_day(){
+       Item standard = new Item("foo", 19, 6) ;
+       GildedRose gr = new GildedRose(new Item[]{standard});
+       gr.updateQuality();
+       assertEquals(standard.sellIn, 18);
+       assertEquals(standard.quality, 5);
+    
+    }
 
 }
