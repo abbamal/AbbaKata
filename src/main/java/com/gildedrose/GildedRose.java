5ee39359d18b1ace1cd80dbe245745e9c270ed5e
abba Malainine <abba.malainine@gmail.com>
2022-07-24 19:11:08 +0100

    first step in break the nasted if in updateQualityItem , change the test order

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -46,13 +46,8 @@ class GildedRose {
     }
 
     public void updateQualityItem(Item item) {
-        if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
+        if (item.name.equals("Aged Brie") || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
                 if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -66,6 +61,12 @@ class GildedRose {
                             item.quality = item.quality + 1;
                         }
                     }
+                }
+            }            
+        } else {
+            if (item.quality > 0) {
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.quality = item.quality - 1;
                 }
             }
         }
