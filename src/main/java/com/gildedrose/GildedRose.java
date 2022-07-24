09e686eb9ec60286e566c0bf754269abb3e4871c
abba Malainine <abba.malainine@gmail.com>
2022-07-24 20:27:09 +0100

    inject inventoryItem instance in our methode parameters

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,22 +10,22 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
              InventoryItem inventoryItem = new InventoryItem(item);
-            updateDaily(item);
+            updateDaily(item,inventoryItem);
         }
     }
 
-    public void updateDaily(Item item) {
-        updateQualityItem(item);
-        updateExpirationDays(item);
+    public void updateDaily(Item item, InventoryItem inventoryItem) {
+        updateQualityItem(item,inventoryItem);
+        updateExpirationDays(item,inventoryItem);
         if (item.sellIn < 0) {
-            expirationProcess(item);
+            expirationProcess(item,inventoryItem);
         }
     }
 
-    public void expirationProcess(Item item) {
+    public void expirationProcess(Item item, InventoryItem inventoryItem) {
             
             if (item.name.equals("Aged Brie")) {
-            encreaseQuality(item);                
+            encreaseQuality(item,inventoryItem);                
                 }
   
             else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert"))  {
@@ -36,7 +36,7 @@ class GildedRose {
                        return ;
                     }
             else {
-            decreaseQuality(item);
+            decreaseQuality(item,inventoryItem);
                 }    
         
     }
@@ -45,7 +45,7 @@ class GildedRose {
 
 
 
-    public void updateExpirationDays(Item item) {
+    public void updateExpirationDays(Item item, InventoryItem inventoryItem) {
         if(item.name.equals("Sulfuras, Hand of Ragnaros")){
             return ;
         }
@@ -54,17 +54,17 @@ class GildedRose {
 
     }
 
-    public void updateQualityItem(Item item) {
+    public void updateQualityItem(Item item, InventoryItem inventoryItem) {
         if (item.name.equals("Aged Brie") ) {
-            encreaseQuality(item);            
+            encreaseQuality(item, inventoryItem);            
         }
         else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            encreaseQuality(item); 
+            encreaseQuality(item, inventoryItem); 
                 if (item.sellIn < 11) {
-                encreaseQuality(item);
+                encreaseQuality(item, inventoryItem);
                 }
                 if (item.sellIn < 6) {
-                encreaseQuality(item);
+                encreaseQuality(item, inventoryItem);
                 }
             
        
@@ -73,16 +73,16 @@ class GildedRose {
           return;
         }
         else {
-            decreaseQuality(item);
+            decreaseQuality(item, inventoryItem);
         }
     }
 
-    public void encreaseQuality(Item item) {
+    public void encreaseQuality(Item item, InventoryItem inventoryItem) {
         if (item.quality < 50) {
             item.quality = item.quality + 1;
         }
     }
-    public void decreaseQuality(Item item) {
+    public void decreaseQuality(Item item, InventoryItem inventoryItem) {
         if (item.quality > 0) {
             item.quality = item.quality - 1;
         }
