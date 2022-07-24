1eaa8b395f7b359b55ea5eaec86f3f81742776e0
abba Malainine <abba.malainine@gmail.com>
2022-07-24 21:41:35 +0100

    overrade decreaseQuality methode in Conjured Class to deal with spcial case of Conjured Type

--- src/main/java/com/gildedrose/Conjured.java
+++ src/main/java/com/gildedrose/Conjured.java
@@ -10,5 +10,12 @@ public class Conjured extends InventoryItem {
     public Conjured(Item item) {
         super(item);
     }
+
+    @Override
+    protected void decreaseQuality() {
+        item.quality = Math.max(0, item.quality-2);
+    }
+    
+    
     
 }
